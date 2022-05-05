4cfb65b0d40cc4b1ca50f31913c5ff4ddcdf7776
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-05-05 12:33:14 +0900

    fix: dynamic array

--- iterator/models/BookShelf.java
+++ iterator/models/BookShelf.java
@@ -1,22 +1,25 @@
 package iterator.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import iterator.interfaces.Aggregate;
 import iterator.interfaces.Iterator;
 
 public class BookShelf implements Aggregate {
-  private Book[] books;
+  private List<Book> books;
   private int last = 0;
 
   public BookShelf(int maxsize) {
-    this.books = new Book[maxsize];
+    this.books = new ArrayList<>(maxsize);
   }
 
   public Book getBookAt(int index) {
-    return books[index];
+    return books.get(index);
   }
 
   public void appendBook(Book book) {
-    this.books[last] = book;
+    this.books.add(book);
     last++;
   }
 
